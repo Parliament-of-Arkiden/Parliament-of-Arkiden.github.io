40ebd194ef8e7ae03d5a3fe7aa7753092898a672
Parliament-of-Arkiden <parliament.arkiden@proton.me>
2026-08-16 11:47:38 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
+const constitutionurl = "./CONSTITUTION/index.md";
+const constitutionmarkdown = await fetch(constitutionurl).then(r => r.text());
+document.getElementById("constitution").innerHTML = marked.parse(constitutionmarkdown);
+        
+const descriptionurl = "https://raw.githubusercontent.com/Parliament-of-Arkiden/Parliament-of-Arkiden.github.io/main/README.md";
+const descriptionmarkdown = await fetch(descriptionurl).then(r => r.text());
+document.getElementById("description").innerHTML = marked.parse(descriptionmarkdown);
+        
+const lawsurl = "https://raw.githubusercontent.com/Parliament-of-Arkiden/Parliament-of-Arkiden.github.io/main/LAWS/PASSED/index.md";
+const lawsmarkdown = await fetch(lawsurl).then(r => r.text());
+document.getElementById("laws").innerHTML = marked.parse(lawsmarkdown);
+       
+const plawsurl = "https://raw.githubusercontent.com/Parliament-of-Arkiden/Parliament-of-Arkiden.github.io/main/LAWS/PROPOSED/index.md";
+const plawsmarkdown = await fetch(plawsurl).then(r => r.text());
+document.getElementById("plaws").innerHTML = marked.parse(plawsmarkdown);
