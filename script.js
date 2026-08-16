2021f61a5529bdd274602859e1c1369e597adbf1
Parliament-of-Arkiden <parliament.arkiden@proton.me>
2026-08-16 11:54:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const constitutionurl = "./CONSTITUTION/index.md";
 const constitutionmarkdown = await fetch(constitutionurl).then(r => r.text());
 document.getElementById("constitution").innerHTML = marked.parse(constitutionmarkdown);
         
-const descriptionurl = "https://raw.githubusercontent.com/Parliament-of-Arkiden/Parliament-of-Arkiden.github.io/main/README.md";
+const descriptionurl = "./README.md";
 const descriptionmarkdown = await fetch(descriptionurl).then(r => r.text());
 document.getElementById("description").innerHTML = marked.parse(descriptionmarkdown);
         
-const lawsurl = "https://raw.githubusercontent.com/Parliament-of-Arkiden/Parliament-of-Arkiden.github.io/main/LAWS/PASSED/index.md";
+const lawsurl = "./LAWS/PASSED/index.md";
 const lawsmarkdown = await fetch(lawsurl).then(r => r.text());
 document.getElementById("laws").innerHTML = marked.parse(lawsmarkdown);
        
-const plawsurl = "https://raw.githubusercontent.com/Parliament-of-Arkiden/Parliament-of-Arkiden.github.io/main/LAWS/PROPOSED/index.md";
+const plawsurl = "./LAWS/PROPOSED/index.md";
 const plawsmarkdown = await fetch(plawsurl).then(r => r.text());
 document.getElementById("plaws").innerHTML = marked.parse(plawsmarkdown);
